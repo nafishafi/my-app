b532c6e4950681103174a6c811614385fc4c95db
Muhammad Nafis Hafi <nafishafi17@students.unnes.ac.id>
2025-05-07 16:29:25 +0700

    Implement product and authentication pages with routing

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -1,4 +1,15 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { useEffect, useState } from "react";
+import { useRouter } from 'next/router';
+
 const ProductPage = () => {
+    const [isLogin, setIsLogin] = useState(false);
+    const { push } = useRouter();
+    useEffect(() => {
+        if(!isLogin) {
+            push("/auth/login");
+        }
+    }, []);
     return (
         <div>
             <h1>Product Page</h1>
